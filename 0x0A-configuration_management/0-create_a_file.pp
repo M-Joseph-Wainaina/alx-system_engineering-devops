bd4352750935c629692b8c1730a5005f1dc80e3c
joseph wainaina <josephynaina@gmail.com>
2023-03-24 14:02:56 +0000

    create a file in puppet

---
+++ 0x0A-configuration_management/0-create_a_file.pp
@@ -0,0 +1,9 @@
+#create a file using puppet
+
+file{'/tmp/school':
+  ensure  => 'present',
+  content => 'I love puppet',
+  owner   => 'www-data',
+  group   => 'www-data',
+  mode    => '0744'
+}
