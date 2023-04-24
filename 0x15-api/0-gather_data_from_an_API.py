84e7d53f4d0991c50449d7a8f67d52c5e5be9f4a
Your Name <you@example.com>
2023-04-24 13:15:35 +0000

    send get request to a rest api

---
+++ 0x15-api/0-gather_data_from_an_API.py
@@ -0,0 +1,39 @@
+#!/usr/bin/python3
+""" send a get request to an api and print the todos"""
+
+import requests
+import sys
+
+if __name__ == '__main__':
+    try:
+        userId = int(sys.argv[1])
+    except ValueError:
+        exit()
+
+    apiUrl = "https://jsonplaceholder.typicode.com"
+    userUrl = f"{apiUrl}/users/{userId}"
+    todosUrl = f"{userUrl}/todos"
+
+    # user details response
+    res = requests.get(userUrl).json()
+    userName = res.get('name')
+
+    # user todos
+    todos = requests.get(todosUrl).json()
+
+    numberOfTasks = len(todos)
+    
+    # get the number of completed tasks
+    completedTasks = 0
+    for todo in todos:
+        if todo.get('completed'):
+            completedTasks += 1
+    incompleteTasks = numberOfTasks - completedTasks
+
+    # printing the output
+
+    str = f"Employee {userName} is done with tasks({completedTasks}/{numberOfTasks}):"
+    print(str)
+    for todo in todos:
+        if todo.get('completed'):
+            print("\t " + todo.get('title'))
